9e0e3c7c80d4aeac8275d0701db330fba7194c9b
bonanashelby <bonanashelby@gmail.com>
2017-05-16 15:47:41 -0700

    worked on sum series for optional parameters and adding in other parameters

--- src/series.py
+++ src/series.py
@@ -15,3 +15,10 @@ def lucas(n):
     for i in range(n - 1):
         x, y = y, x + y
     return x
+
+
+def sum_series(n, x=0, y=1):
+    """Function that uses a required parameter and two optional parameters."""
+    for i in range(n - 1):
+        x, y = y, x + y
+    return x
--- src/test_series.py
+++ src/test_series.py
@@ -16,13 +16,6 @@ FIB_TABLE = [
 ]
 
 
-@pytest.mark.parametrize('n, result', FIB_TABLE)
-def test_fibonacci(n, result):
-    """Test fibonacci functions."""
-    from series import fibonacci
-    assert fibonacci(n) == result
-
-
 LUCAS_TABLE = [
     (1, 2),
     (2, 1),
@@ -37,8 +30,36 @@ LUCAS_TABLE = [
 ]
 
 
+SUM_TABLE = [
+    (5, 2, 1, 7),
+    (7, 2, 1, 18),
+    (10, 2, 1, 76)
+]
+
+
+@pytest.mark.parametrize('n, result', FIB_TABLE)
+def test_fibonacci(n, result):
+    """Test fibonacci functions."""
+    from series import fibonacci
+    assert fibonacci(n) == result
+
+
 @pytest.mark.parametrize('n, result', LUCAS_TABLE)
 def test_lucas(n, result):
     """Test Lucas function."""
     from series import lucas
     assert lucas(n) == result
+
+
+@pytest.mark.parametrize('n, result', FIB_TABLE)
+def test_sum_series_fib(n, result):
+    """Test sum series."""
+    from series import sum_series
+    assert sum_series(n) == result
+
+
+@pytest.mark.parametrize('n, x, y, result', SUM_TABLE)
+def test_sum_series_opt(n, x, y, result):
+    """Test for optional sum series."""
+    from series import sum_series
+    assert sum_series(n, x, y) == result
